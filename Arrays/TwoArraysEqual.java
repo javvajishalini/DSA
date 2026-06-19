c66209055612832ca5422406341221ece5435d2e
Shalini <shalinijavvaji06@gmail.com>
2026-06-19 20:45:56 +0530

    Add checking equality of two arrays

---
+++ Arrays/TwoArraysEqual.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class TwoArraysEqual{
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(2,3,4));
+        ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList(2,3,4));
+        
+       
+        System.out.println(arr.equals(arr1));
+    }
+}
