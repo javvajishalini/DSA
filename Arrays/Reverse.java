08f003e2c0c76c97f78952ca7775e1dba398292a
Shalini <shalinijavvaji06@gmail.com>
2026-06-19 20:39:40 +0530

    Add array reversal program

---
+++ Arrays/Reverse.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+class Reverse{
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+        
+      Collections.reverse(arr);
+      System.out.println(arr);
+}
+}
