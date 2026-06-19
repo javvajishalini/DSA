2f79afe01ad5bac4bac34fc2a44381921eea6bfe
Shalini <shalinijavvaji06@gmail.com>
2026-06-19 20:40:36 +0530

    Add array sorting implementation

---
+++ Arrays/Sort.java
@@ -0,0 +1,11 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Sort{
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(9,4,7,3));
+        
+      Collections.sort(arr);
+      System.out.println(arr);
+}
+}
