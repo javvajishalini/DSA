520bc032b0258cdc1ee3d2c3723034aaf391ad80
Shalini <shalinijavvaji06@gmail.com>
2026-07-02 00:04:21 +0530

    Palindrome Number

---
+++ Numbers/Palindrome.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+class Palindrome {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp = n;
+        int rev = 0;
+
+        while (temp > 0) {
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        if (rev == n) {
+            System.out.print("Palindrome Number ");
+        } else {
+            System.out.print("Not Palindrome Number");
+        }
+    }
+}
