1e65202be1bb58e31a4ebd32eb06ed66b5333e1a
Shalini <shalinijavvaji06@gmail.com>
2026-07-14 22:45:51 +0530

    Reverse a number program

--- Numbers/ReverseNumber.java
+++ Numbers/ReverseNumber.java
@@ -1,18 +1,34 @@
 
 import java.util.Scanner;
 
+// Program to reverse a given number
+// Example: Input: 12345 → Output: 54321
+//          Input: -678  → Output: -876
+
 class ReverseNumber {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    // Iterative approach to reverse a number
+    static int reverse(int n) {
+        boolean isNegative = n < 0;
+        if (isNegative) n = -n;
 
-        int n = sc.nextInt(), rev = 0;
-
+        int rev = 0;
         while (n > 0) {
             rev = rev * 10 + n % 10;
             n /= 10;
         }
 
-        System.out.println(rev);
+        return isNegative ? -rev : rev;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Reversed number: " + reverse(n));
+
+        sc.close();
     }
 }
